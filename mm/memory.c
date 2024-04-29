6c9748f26396c952af9ae17af889c6bdbf121e03
Alexander Plaikner <alexander.plaikner@tum.de>
2024-04-29 11:26:26 +0200

    Limit anon folios to order 3

--- mm/memory.c
+++ mm/memory.c
@@ -4177,7 +4177,7 @@ static struct folio *alloc_anon_folio(struct vm_fault *vmf)
 	unsigned long addr;
 	pte_t *pte;
 	gfp_t gfp;
-	int order;
+	int order=3;
 
 	/*
 	 * If uffd is active for the vma we need per-page fault fidelity to
@@ -4207,6 +4207,7 @@ static struct folio *alloc_anon_folio(struct vm_fault *vmf)
 	 * pte_none(). Note that all remaining orders will be completely
 	 * pte_none().
 	 */
+/*
 	order = highest_order(orders);
 	while (orders) {
 		addr = ALIGN_DOWN(vmf->address, PAGE_SIZE << order);
@@ -4214,7 +4215,7 @@ static struct folio *alloc_anon_folio(struct vm_fault *vmf)
 			break;
 		order = next_order(&orders, order);
 	}
-
+*/
 	pte_unmap(pte);
 
 	/* Try allocating the highest of the remaining orders. */
@@ -4226,7 +4227,7 @@ static struct folio *alloc_anon_folio(struct vm_fault *vmf)
 			clear_huge_page(&folio->page, vmf->address, 1 << order);
 			return folio;
 		}
-		order = next_order(&orders, order);
+//		order = next_order(&orders, order);
 	}
 
 fallback:
